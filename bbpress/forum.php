9dcef9327ca937f939f3b08f82f3b2c6485bb673
Mladen Šćekić <info@sceko.com>
2015-04-28 11:37:38 +0200

    Update

--- bbpress/forum.php
+++ bbpress/forum.php
@@ -8,21 +8,12 @@
 <meta name="revisit-after" content="1 days">
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/bbpress/style.css" type="text/css" media="screen">
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/bbpress/css/bbpress.css" type="text/css" media="screen">
-
-
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
-
-
 <?php wp_head(); ?>
 </head>
-
-<body <?php body_class(); ?> >
-
+<body <?php body_class(); ?>>
 <nav class="navbar navbar-default navbar-fixed-top">
 <div class="container">
-
 <div class="navbar-header">
 <button type="button" class="navbar-toggle collapsed animated tada" id="btn" data-toggle="collapse" data-target="#meni">Meni <i class="fa fa-bars"></i></button>
 <?php if (is_single('')) : ?>
@@ -30,9 +21,7 @@
 <?php endif; ?>
 <a class="navbar-brand animated fadeInDown" id="limun" href="<?php echo esc_url(home_url()); ?>/forum" title="<?php bloginfo('description'); ?>"><?php bloginfo('name'); ?> Forum</a>
 </div>
-
 <div class="collapse navbar-collapse" id="meni">
-
 <ul class="nav navbar-nav hidden-lg hidden-sm">
 <?php if (is_user_logged_in()) : ?>
 <a href="#new-post" class="zapocni btn navbar-btn pull-left btn-success" style="margin-left:15px;margin-right:15px;">Započni temu</a>
@@ -44,16 +33,13 @@
 </div>
 <?php endif; ?>
 </ul>
-
 <ul class="nav navbar-nav pull-right hidden-xs hidden-sm">
-
 <form role="search" method="get" id="bbp-searchform" action="<?php echo esc_url( home_url( 'forum/' ) ); ?>" class="navbar-form  navbar-left">
 <div class="form-group has-feedback has-feedback-left">
 <input data-toggle="tooltip" data-placement="bottom" title="Pronađite željenu temu" type="text" name="ts" id="ts" class="form-control" placeholder="Pretraga...">
 <span class="fa fa-search form-control-feedback" aria-hidden="true"></span>
 </div>
 </form>
-
 <div class="btn-group navbar-left" role="group">
 <button type="button" class="btn navbar-btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-bars"></i></button>
 <ul class="dropdown-menu dropdown-menu-right" role="menu">
@@ -62,9 +48,7 @@
 <li<?php if (is_single('pomoc')) { echo ' class="active"'; } ?>><a href="<?php echo esc_url(home_url()); ?>/pomoc"><i class="fa fa-life-ring"></i> Pomoć</a></li>
 </ul>
 </div>
-
 <?php if (is_user_logged_in()) : ?>
-
 <li class="dropdown navbar-right">
 <a href="#" class="dropdown-toggle navbar-gravatar" data-toggle="dropdown"> <?php global $current_user; get_currentuserinfo(); echo get_avatar($current_user->user_email, 32 ); ?></a>
 <ul class="dropdown-menu" role="menu" >
@@ -74,21 +58,17 @@
 <li><a href="<?php echo wp_logout_url(); ?>"><i class="fa fa-sign-out"></i> Odjavi se</a></li>
 </ul>
 </li>
-
 <?php else : ?>
 <div class="btn-group pull-right navbar-btn" style="margin-left:15px;">
 <a class="btn btn-default" href="" data-toggle="modal" data-target="#prijava">Prijava</a>
 <a class="btn btn-success" href="" data-toggle="modal" data-target="#registracija">Registracija</a>
 </div>
 <?php endif; ?>
-
 </ul>
-
 </div>
 </div>
 </nav>
-
-<?php if (is_single('')) : ?>
+<?php if (is_single()) : ?>
 <div class="header">
 <div class="container">
 <div class="row">
@@ -99,14 +79,9 @@
 </div>
 </div>
 <?php endif; ?>
-
-
 <div class="container" style="margin-top:40px;">
-
-
 <div class="row">
 <?php if (is_archive() ||  bbp_is_single_forum() || bbp_is_single_view()) : ?>
-
 <div class="col-md-2">
 <ul class="nav-pills nav-stacked hidden-xs hidden-sm" data-spy="affix">
 <?php if ( is_user_logged_in() ) : ?>
@@ -117,26 +92,14 @@
 <li><a href="<?php echo bbp_get_user_profile_url( get_current_user_id() ); ?>omiljene/" class="hidden-xs hidden-sm"><i class="fa fa-star"></i> Omiljene teme</a></li>
 </ul>
 </div>
-
 <div class="col-md-10">
 <article><?php the_content(); ?></article>
 </div>
-
 <?php else : ?>
-
 <div class="col-md-12"><article><?php the_content(); ?></article></div>
-
 <?php endif; ?>
-
-<div class="col-md-12"><div class="copyright text-right"><a href="http://www.sceko.com" target="_blank" title="Izrada sajtova">Created by Sceko</a></div></div>
+<div class="col-md-12"><div class="copyright text-right"><a href="<?php echo esc_url(home_url()); ?>/forum" title="Powered by bbPress">Powered by bbPress</a></div></div>
 </div>
-
-
-
-
-
-
-
 <?php if (is_user_logged_in()) : ?>
 <?php else : ?>
 <div class="modal fade" id="prijava" tabindex="-1" role="dialog" aria-labelledby="prijava" aria-hidden="true">
@@ -161,13 +124,10 @@
 <button type="submit" name="wp-submit" style="width:100%;" class="btn btn-primary">Prijava</button>
 </div>
 </form>
-
 </div>
 </div>
 </div>
 </div> 
-
-
 <div class="modal fade" id="registracija" tabindex="-1" role="dialog" aria-labelledby="registracija" aria-hidden="true">
 <div class="modal-dialog modal-sm">
 <div class="modal-content text-center">
@@ -190,42 +150,36 @@
 <input type="hidden" name="redirect_to" value="<?php echo $_SERVER['REQUEST_URI']; ?>?register=true" />
 <input type="hidden" name="user-cookie" value="1">
 </form>
-
 </div>
 </div>
 </div>
 </div>
 <?php endif; ?>
-
 <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-
 <script type="text/javascript">
 $(document).ready(
-    function() {
-        $(".bbp-topic-reply-link").click(function() {
-        $("#new-post").fadeToggle();
-        });
-        $(".bbp-reply-to-link").click(function() {
-        $("#new-post").fadeToggle();
-        });
-        $(".zapocni").click(function() {
-        $("#new-post").fadeToggle();
-        });
-        $("body").mouseup(function(e) {
-        var subject = $("#new-post"); 
-        if(e.target.id != subject.attr('id') && !subject.has(e.target).length) { subject.fadeOut(); }
-    });
+function() {
+$(".bbp-topic-reply-link").click(function() {
+$("#new-post").fadeToggle();
+});
+$(".bbp-reply-to-link").click(function() {
+$("#new-post").fadeToggle();
+});
+$(".zapocni").click(function() {
+$("#new-post").fadeToggle();
+});
+$("body").mouseup(function(e) {
+var subject = $("#new-post"); 
+if(e.target.id != subject.attr('id') && !subject.has(e.target).length) { subject.fadeOut(); }
+});
 });
 </script>
-
 <script type="text/javascript">
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+$('[data-toggle="tooltip"]').tooltip()
 });
 </script>
-
 <?php wp_footer(); ?>
-
 </body>
 </html>
--- bbpress/loop-single-topic.php
+++ bbpress/loop-single-topic.php
@@ -8,20 +8,15 @@
  */
 
 ?>
-
-
 <ul id="bbp-topic-<?php bbp_topic_id(); ?>">
 <li style="margin-bottom:30px;">
-
 <div class="row">
-
 <div class="col-md-10">
 <div class="pull-left" style="margin-right:25px;">
 <?php do_action( 'bbp_theme_before_topic_freshness_author' ); ?>
 <?php bbp_author_link( array( 'post_id' => bbp_get_topic_last_active_id(), 'show_role' => false, 'type' => 'avatar', 'size' => '48' ) ); ?>
 <?php do_action( 'bbp_theme_after_topic_freshness_author' ); ?>
 </div>
-
 <div class="naslov">
 <header>
 <?php do_action( 'bbp_theme_before_topic_title' ); ?> 
@@ -32,20 +27,16 @@
 <?php do_action( 'bbp_theme_after_topic_meta' ); ?>
 </header>
 </div>
-
 <div class="autor">
 <span style="font-weight:600;"><?php bbp_author_link( array( 'post_id' => bbp_get_topic_last_active_id(), 'show_role' => false, 'type' => 'name' ) ); ?></span> pre <?php bbp_topic_freshness_link(); ?> 
 </div>
 </div>
-
 <div class="col-md-2">
 <span class="pull-right text-center hidden-xs hidden-sm">
 <span class="broj"><?php bbp_show_lead_topic() ? bbp_topic_reply_count() : bbp_topic_post_count(); ?></span><br>
 <p class="odgovora">odgovora<p>
 </span>
 </div>
-
 </div>
-
 </li>
 </ul><!-- #bbp-topic-<?php bbp_topic_id(); ?> -->
