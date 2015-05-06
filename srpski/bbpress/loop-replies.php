ce67425c826382d4e7d298f99dbb4078afd56b7c
Mladen Šćekić <info@sceko.com>
2015-05-06 11:14:43 +0200

    Update

---
+++ srpski/bbpress/loop-replies.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Replies Loop
+ *
+ * @package bbPress
+ * @subpackage Theme
+ */
+
+?>
+<?php do_action( 'bbp_template_before_replies_loop' ); ?>
+<ul id="topic-<?php bbp_topic_id(); ?>-replies">
+<li class="bbp-body">
+<?php if ( bbp_thread_replies() ) : ?>
+<?php bbp_list_replies(); ?>
+<?php else : ?>
+<?php while ( bbp_replies() ) : bbp_the_reply(); ?>
+<?php bbp_get_template_part( 'loop', 'single-reply' ); ?>
+<?php endwhile; ?>
+<?php endif; ?>
+</li><!-- .bbp-body -->
+</ul><!-- #topic-<?php bbp_topic_id(); ?>-replies -->
+<?php do_action( 'bbp_template_after_replies_loop' ); ?>
---
+++ srpski/bbpress/loop-single-topic.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Topics Loop - Single
+ *
+ * @package bbPress
+ * @subpackage Theme
+ */
+
+?>
+<ul id="bbp-topic-<?php bbp_topic_id(); ?>">
+<li style="margin-bottom:30px;">
+<div class="row">
+<div class="col-md-10">
+<div class="pull-left" style="margin-right:25px;">
+<?php do_action( 'bbp_theme_before_topic_freshness_author' ); ?>
+<?php bbp_author_link( array( 'post_id' => bbp_get_topic_last_active_id(), 'show_role' => false, 'type' => 'avatar', 'size' => '48' ) ); ?>
+<?php do_action( 'bbp_theme_after_topic_freshness_author' ); ?>
+</div>
+<div class="naslov">
+<header>
+<?php do_action( 'bbp_theme_before_topic_title' ); ?> 
+<a <?php bbp_topic_class(); ?> href="<?php bbp_topic_permalink(); ?>" title="<?php bbp_topic_title(); ?>"><?php bbp_topic_title(); ?></a>
+<?php do_action( 'bbp_theme_after_topic_title' ); ?>
+<?php bbp_topic_pagination(); ?>
+<?php do_action( 'bbp_theme_before_topic_meta' ); ?>
+<?php do_action( 'bbp_theme_after_topic_meta' ); ?>
+</header>
+</div>
+<div class="autor">
+<span style="font-weight:600;"><?php bbp_author_link( array( 'post_id' => bbp_get_topic_last_active_id(), 'show_role' => false, 'type' => 'name' ) ); ?></span> pre <?php bbp_topic_freshness_link(); ?> 
+</div>
+</div>
+<div class="col-md-2">
+<div class="pull-right text-center hidden-xs hidden-sm">
+<span class="broj"><?php bbp_show_lead_topic() ? bbp_topic_reply_count() : bbp_topic_post_count(); ?></span>
+<p class="odgovora">odgovora</p>
+</div>
+</div>
+</div>
+</li>
+</ul><!-- #bbp-topic-<?php bbp_topic_id(); ?> -->
